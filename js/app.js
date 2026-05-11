05abbd7acd2eb9a6a837f235385099dbb18393eb
DK Mobile Workstation <wenger.dk@gmail.com>
2026-05-11 17:05:20 -0500

    Filtering done, sort icons function, sorting half implemented (need to functionize the sort itself)

--- js/app.js
+++ js/app.js
@@ -17,13 +17,35 @@ console.log("\n\n");
 //#endregion ==================================================================================
 
 
+//#Region Define Some Scoped Variables ========================================================
+const newAlbumFormEle = document.getElementById('album-form');
+const albumFormatEle = document.getElementById('album-format');
+const albumSourceEle = document.getElementById('album-source');
+const albumSourceHiderEle = document.getElementById('album-source-hider');
+const renderResultsEle = document.getElementById('render-results');
+const sortTitleIconEle = document.getElementById('sort-title-icon');
+const sortArtistIconEle = document.getElementById('sort-artist-icon');
+const sortYearIconEle = document.getElementById('sort-year-icon');
+const sortStatusIconEle = document.getElementById('sort-status-icon');
+
+let collection = [];
+
+let filterSearchSetting = '';
+let filterFormatSetting = 'all';
+
+let weSortedTitle = false;
+let weSortedArtist = false;
+let weSortedYear = false;
+let weSortedStatus = false;
+
+//#EndRegion ==================================================================================
+
+
 //#Region Imports and Setups ==================================================================
 import { albumId, setAlbumId } from './models/album.js';
 import { CDAlbum } from './models/albumcd.js';
 import { DigitalAlbum } from './models/albumdigital.js';
 import { saveCollection, loadCollection } from './localstorage.js';
-
-let collection = [];
 
 // Load from localStorage but do so with a try/fail in case there is no data or it's corrupted
 try {
@@ -43,11 +65,11 @@ if (collection.length < 12) {
         new DigitalAlbum("Early Recordings", "Justin Hinds and the Dominoes", 1965, "https://m.media-amazon.com/images/I/61c5s6GdlEL._UX716_FMwebp_QL85_.jpg", "Amazon"),
         new CDAlbum("Welcome to the Pleasuredome", "Frankie Goes to Hollywood", 1984, "https://upload.wikimedia.org/wikipedia/en/0/0e/Welcome_To_The_Pleasuredome.jpg", true),
         new CDAlbum("Pop Goes The World", "Men Without Hats", 1987, "https://upload.wikimedia.org/wikipedia/en/a/a2/Men_Without_Hats-Pop_Goes_The_World.jpg", false),
-        new CDAlbum("Delicate Sound of Thunder", "Pink Floyd", 1998, "https://upload.wikimedia.org/wikipedia/en/6/6b/Dsothunder-250.jpg", true),
+        new CDAlbum("Delicate Sound of Thunder", "Pink Floyd", 1998, "https://upload.wikimedia.org/wikipedia/en/6/6b/Dsothunder-250.jpg", false),
         new DigitalAlbum("Disintegration", "The Cure", 1989, "https://upload.wikimedia.org/wikipedia/en/b/b8/CureDisintegration.jpg", "Amazon"),
         new DigitalAlbum("Find a Way Home", "MXPX", 2023, "https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Find_a_Way_Home.jpg/250px-Find_a_Way_Home.jpg", "Bandcamp"),
         new CDAlbum("Revolver", "The Beatles", 1966, "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Revolver_%28album_cover%29.jpg/250px-Revolver_%28album_cover%29.jpg", false),
-        new DigitalAlbum("LaTour", "LaTour", 1991, "https://upload.wikimedia.org/wikipedia/en/8/84/LaTouralbumcover.jpg", "Converted"),
+        new DigitalAlbum("LaTour", "LaTour", 1991, "https://upload.wikimedia.org/wikipedia/en/8/84/LaTouralbumcover.jpg", "Amazon"),
         new DigitalAlbum("Borders & Boundaries", "Less Than Jake", 2000, "https://upload.wikimedia.org/wikipedia/en/e/e2/LTJ-Borders-boundaries.jpg", "Bandcamp")
     ];
 
@@ -59,33 +81,33 @@ if (collection.length < 12) {
 //#EndRegion ==================================================================================
 
 
-//#Region Define Element Variables ============================================================
-const newAlbumFormEle = document.getElementById('album-form');
-const albumFormatEle = document.getElementById('album-format');
-const albumSourceEle = document.getElementById('album-source');
-const albumSourceHiderEle = document.getElementById('album-source-hider');
-const renderResultsEle = document.getElementById('render-results');
-//#EndRegion ==================================================================================
+//#Region Functions ===========================================================================
 
-//#Region Render Function =====================================================================
-function renderList(data = collection) {
+// Render Function ----------------------------------------------------------------------------
+function renderList() {
 
     renderResultsEle.innerHTML = '';
 
     // Method .forEach replaces our for loop 
-    data.forEach(function (album) {
+    collection.forEach(function (album) {
 
-        const extraInfo = album.format === "cd"
-            // a ternary expression shortens the if... else statement
-            // condition ? expressionIfTrue : expressionIfFalse;
-            ? `<span class="badge ${album.isRipped ? 'bg-info' : 'bg-warning'}">
+        if (
+            (filterFormatSetting === 'all' || album.format === filterFormatSetting) &&
+            (filterSearchSetting === '' ||
+                album.title.toLowerCase().includes(filterSearchSetting) ||
+                album.artist.toLowerCase().includes(filterSearchSetting))) {
+
+            const extraInfo = album.format === "cd"
+                // a ternary expression shortens the if... else statement
+                // condition ? expressionIfTrue : expressionIfFalse;
+                ? `<span class="badge ${album.isRipped ? 'bg-info' : 'bg-warning'}">
             <input type="checkbox" data-id="${album.id}" ${album.isRipped ? 'checked' : ''}> Converted</span>`
-            : `<span class="badge bg-info">${album.source}</span>`;
+                : `<span class="badge bg-info">${album.source}</span>`;
 
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
             <td class="d-none d-md-table-cell">
-                <img src="${album.coverUrl}" class="rounded"></td>
+                <img src="${album.coverUrl}" class="rounded border"></td>
             <td class="fw-bold">${album.title}</td>
             <td>${album.artist}</td>
             <td>${album.year}</td>
@@ -96,12 +118,51 @@ function renderList(data = collection) {
                 </button><button class="btn btn-sm edit-btn" data-id="${album.id}">
                     <i class="bi bi-pencil-square"></i>
                 </button>
-            </td>
-        `;
-        renderResultsEle.appendChild(tr);
+            </td>`;
+            renderResultsEle.appendChild(tr);
+        };
     });
+};
+
+// Update Sort Icons --------------------------------------------------------------------------
+
+function weSorted(target, direction) {
+
+    sortTitleIconEle.innerHTML = '';
+    sortArtistIconEle.innerHTML = '';
+    sortYearIconEle.innerHTML = '';
+    sortStatusIconEle.innerHTML = '';
+
+    weSortedTitle = false;
+    weSortedArtist = false;
+    weSortedYear = false;
+    weSortedStatus = false;
+
+    switch (target) {
+        case 'title':
+            sortTitleIconEle.innerHTML = `<i class="bi bi-arrow-${direction}"></i>`;
+            weSortedTitle = true;
+            break;
+        case 'artist':
+            sortArtistIconEle.innerHTML = `<i class="bi bi-arrow-${direction}"></i>`;
+            weSortedArtist = true;
+            break;
+        case 'year':
+            sortYearIconEle.innerHTML = `<i class="bi bi-arrow-${direction}"></i>`;
+            weSortedYear = true;
+            break;
+        case 'status':
+            sortStatusIconEle.innerHTML = `<i class="bi bi-arrow-${direction}"></i>`;
+            weSortedStatus = true;
+            break;
+        default:
+            break;
+    }
+
 }
+
 //#EndRegion ==================================================================================
+
 
 //#Region New/Edit Album Form =================================================================
 
@@ -115,13 +176,15 @@ albumFormatEle.addEventListener('change', function (e) {
 });
 
 // Add New Item with Validation ---------------------------------------------------------------
-newAlbumFormEle.addEventListener('submit', (e) => {
+newAlbumFormEle.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    // RegExp Validation (Requirement: Title/Artist must start with Letter, 2+ chars)
+    // RegExp Validation (Requirement: Title/Artist must start with Letter, 1+ chars)
+    const titleVal = document.getElementById('album-title').value;
+    const artistVal = document.getElementById('album-artist').value;
+    const yearVal = document.getElementById('album-year').value;
+    const coverURLVal = document.getElementById('album-cover').value;
     const nameRegex = /^[A-Za-z0-9\s]{1,}$/;
-    const titleVal = document.getElementById('title').value;
-    const artistVal = document.getElementById('artist').value;
 
     if (!nameRegex.test(titleVal) || !nameRegex.test(artistVal)) {
         alert("Please enter a valid Title and Artist (at least 1 character).");
@@ -129,22 +192,22 @@ newAlbumFormEle.addEventListener('submit', (e) => {
     }
 
     let newAlbum;
-    const runtime = document.getElementById('runtime').value;
-    const cover = document.getElementById('cover').value;
 
     if (formatSelect.value === "cd") {
-        newAlbum = new CDAlbum(titleVal, artistVal, runtime, cover, document.getElementById('isRipped').checked);
+        newAlbum = new CDAlbum(titleVal, artistVal, yearVal, coverURLVal, document.getElementById('isRipped').checked);
     } else {
-        newAlbum = new DigitalAlbum(titleVal, artistVal, runtime, cover, document.getElementById('source').value);
+        newAlbum = new DigitalAlbum(titleVal, artistVal, yearVal, coverURLVal, document.getElementById('source').value);
     }
 
     collection.push(newAlbum);
     saveCollection(collection);
-    form.reset();
+    this.reset();
     renderList();
 });
 
 //#EndRegion ==================================================================================
+
+
 
 //#Region Table Body Actions ===================================================================
 
@@ -153,10 +216,9 @@ document.getElementById('render-results').addEventListener('click', function (e)
     const deleteAlbum = e.target.closest('.bi-x-circle-fill');
 
     // UX Note for Patrick:
-    // I thought about putting the click event on the whole Ripped badge instead of just the checkbox,
-    // But since the whole line is clickable to get album details, I wanted the conversion to be 
-    // a little harder to trigger... so this is a deliberate choice, and since the feedback of status
-    // is instant, a mis-click can be easily reversed.
+    // I thought about putting the click event on the whole 'Converted' badge instead of just the checkbox,
+    // But I wanted the conversion to be a little harder to trigger... the conversion is a one time event
+    // that would only be done once, so this is a deliberate choice. 
     if (convertAlbum) {
         const id = Number(convertAlbum.dataset.id);
         const index = collection.findIndex(function (album) {
@@ -190,24 +252,64 @@ document.getElementById('render-results').addEventListener('click', function (e)
 
 //#EndRegion ==================================================================================
 
+
+
 //#Region Sorting and Filtering Listeners =====================================================
-// Sorting and Filtering
-document.getElementById('filter-search').addEventListener('input', (e) => {
-    const term = e.target.value.toLowerCase();
-    const filtered = collection.filter(a =>
-        a.title.toLowerCase().includes(term) || a.artist.toLowerCase().includes(term)
-    );
-    renderList(filtered);
+
+// Sorting ------------------------------------------------------------------------------------
+document.getElementById('sort-title').addEventListener('click', function (e) {
+    switch (weSortedTitle) {
+        case false:
+            collection.sort(function (a, b) {
+                if (a.title.toLowerCase() < b.title.toLowerCase()) { return -1; }
+                else if (a.title.toLowerCase() > b.title.toLowerCase()) { return 1; }
+                else { return 0; }
+            })
+            weSorted('title', 'down');
+            break;
+
+        default: 
+            collection.reverse();
+            weSorted('title', 'up');
+            weSortedTitle = false;
+            break;
+    };
+    renderList();
 });
 
-document.getElementById('filter-format').addEventListener('change', (e) => {
-    const val = e.target.value;
-    const filtered = val === 'all' ? collection : collection.filter(a => a.format === val);
-    renderList(filtered);
+document.getElementById('sort-artist').addEventListener('click', function (e) {
+    switch (weSortedArtist) {
+        case false:
+            collection.sort(function (a, b) {
+                if (a.artist.toLowerCase() < b.artist.toLowerCase()) { return -1; }
+                else if (a.artist.toLowerCase() > b.artist.toLowerCase()) { return 1; }
+                else { return 0; }
+            })
+            weSorted('artist', 'down');
+            break;
+
+        default: 
+            collection.reverse();
+            weSorted('artist', 'up');
+            weSortedArtist = false;
+            break;
+    };
+    renderList();
 });
 
+// Filters ------------------------------------------------------------------------------------
+document.getElementById('filter-search').addEventListener('input', function (e) {
+    filterSearchSetting = e.target.value.toLowerCase();
+    renderList();
+});
 
+document.getElementById('filter-format').addEventListener('change', function (e) {
+    filterFormatSetting = e.target.value;
+    renderList();
+});
 //#EndRegion ==================================================================================
+
+
 
 //#region Runtime let's get started! ==========================================================
 console.group("---------- Runtime ----------");
